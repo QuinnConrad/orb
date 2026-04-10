f22623299ebce7ad73338a14edd1e6ed51b5bf20
Quinn Conrad <qconrad@purdue.edu>
2026-04-10 16:56:18 +0000

    Token definitions

---
+++ src/token.rs
@@ -0,0 +1,31 @@
+pub enum Token {
+  Identifier(String),
+  Punctuator(String),
+  Keyword(KeywordKind),
+  Str(String),
+  Num(NumKind),
+  Eof,
+}
+
+pub enum NumKind {
+  Int(i64),
+  Float(f64),
+}
+
+pub enum KeywordKind {
+  Spell,            // Function
+  Arcanum,          // int
+  Halfling,         // float
+  Glyph,            // char
+  Potion,           // struct
+  Rune,             // enum
+  Alignment,        // bool
+  Changeling,       // union
+  Impure,           // signed
+  Pure,             // unsigned
+  Great,            // long
+  Perhaps,          // if
+  Otherwise,        // else
+  Evoke,            // return
+  Void,             // void
+}
