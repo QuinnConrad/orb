351bdce568a60c61a66ed8396fddde177050e913
QuinnConrad <quinnjconrad@gmail.com>
2026-08-08 19:34:41 -0400

    actually changes from last time 😭🙏

---
+++ src/analyzer.rs
@@ -0,0 +1,8 @@
+/**
+ * TODO: The semantic analyzer takes in an AST (vec of stmts)
+ * and deduces if they represent a valid program (declaration before use,
+ * single definitions for values, etc.) and then throws an error or
+ * returns a false value or something if it does not. Anything
+ * that gets past semantic analysis should be a valid
+ * program ready to be sent to the codegen stage.
+ */
---
+++ src/codegen.rs
@@ -0,0 +1,6 @@
+/**
+ * TODO: the codegen accepts an AST (repr. by a vec of Stmts) and
+ * returns LLVM IR representing the machine code of the program.
+ * From there, common tools can be used to optimize & compile
+ * to system architecture.
+ */
